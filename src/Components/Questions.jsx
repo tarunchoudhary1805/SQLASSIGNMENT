ef67221b114d518bd4426e459da9e2c3d5b3ff06
Tarun <tarunchoudhary1805@gmail.com>
2021-04-29 20:27:06 +0530

    loader

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -2,16 +2,28 @@ import React, { useState, useEffect } from "react";
 
 const Questions = () => {
   const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
   useEffect(async () => {
+    setLoading(true);
     let response = await fetch(
       "https://sqlassignmentapi.herokuapp.com/"
     ).then((res) => res.json());
     setData(response);
+    setLoading(false);
   }, []);
   console.log(data);
   return (
     <div className="container">
       <h2 className="text-center">Students Questions List</h2>
+      <div className="text-center">
+
+      {loading && (
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      )}
+      </div>
+      <br/>
       {data?.length === 0 && (
         <h4 className="text-center"> No Questions found</h4>
       )}
@@ -33,7 +45,11 @@ const Questions = () => {
           </>
         </div>
       ))}
-      {data?.length != 0 && <button className="btn btn-success " onClick={window.print}>Print</button>}
+      {data?.length != 0 && (
+        <button className="btn btn-success " onClick={window.print}>
+          Print
+        </button>
+      )}
     </div>
   );
 };
